399f7fb2b8c939fbb89d9a5fd2e90a9825450ca4
Thunderk3g <diwakar29buddy@gmail.com>
2022-01-29 01:19:30 +0530

    table-changes

--- krypto-tracker/src/app/home/home.component.ts
+++ krypto-tracker/src/app/home/home.component.ts
@@ -9,23 +9,38 @@ import millify from 'millify';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  
+  dropDownList: any;
   data: any;
-  li: any;
+  li: any[] = [];
+  item: any;
   totalMarketCap:any;
-  
+  totalMarkets:any;
+  totalCoins: any;
+  totalExchanges: any;
   constructor(private apiService: ApiService , private http: HttpClient ) { 
-    this.li = {};
+    this.li = [];
   }
 
   ngOnInit() {
     this.apiService.getData().subscribe((data) => {
-      this.li = data.body.data.stats;
+      this.li = data.body.data.coins;
       this.totalMarketCap = millify(data.body.data.stats.totalMarketCap, {
         precision: 5,  
         decimalSeparator: "."
       });
-      console.log(this.totalMarketCap);
+      this.totalMarkets = millify(data.body.data.stats.totalMarkets, {
+        precision: 2,  
+        decimalSeparator: "."
+      });
+      this.totalCoins = millify(data.body.data.stats.totalCoins, {
+        precision: 2,  
+        decimalSeparator: "."
+      });
+      this.totalExchanges = millify(data.body.data.stats.totalExchanges, {
+        precision: 2,  
+        decimalSeparator: "."
+      });
+      console.log(this.li);
     });
   
 }
