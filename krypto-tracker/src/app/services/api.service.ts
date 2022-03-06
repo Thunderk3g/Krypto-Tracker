e20b442e0ada862e713ba16d1dd27f882a31b279
Thunderk3g <diwakar29buddy@gmail.com>
2022-03-06 13:14:38 +0530

    Changes

--- krypto-tracker/src/app/services/api.service.ts
+++ krypto-tracker/src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable} from 'rxjs';
 
-const SERVER_API = 'http://localhost:3000/';
+const SERVER_API = 'https://krypto-serv.herokuapp.com/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
