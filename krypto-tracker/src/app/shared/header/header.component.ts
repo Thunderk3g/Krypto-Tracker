c2ac5dc13e7ceadddbbb2171c5988812f854efee
Thunderk3g <diwakar29buddy@gmail.com>
2022-03-09 20:48:14 +0530

    Fix

--- krypto-tracker/src/app/shared/header/header.component.ts
+++ krypto-tracker/src/app/shared/header/header.component.ts
@@ -4,31 +4,31 @@ import { TokenStorageService } from '../../services/token-storage.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   menu: boolean = false;
   currentUser: any;
-  username:any;
+  username: any;
   tokenStorageService: any;
   isOpenedList: any;
+
+  constructor(private token: TokenStorageService) {}
+
+  ngOnInit() {
+    this.currentUser = this.token.getUser();
+  }
+
   openMenu(source: any) {
     this.isOpenedList = source;
-}
-closeMenu() {
+  }
+  closeMenu() {
     this.isOpenedList = -1;
-}
+  }
   menuDropdown() {
     this.menu = !this.menu;
   }
-  constructor(private token: TokenStorageService) { }
-
-  ngOnInit(){
-    this.currentUser = this.token.getUser();
-    this.username = this.currentUser.email.match(/(.+)@/)[1]
-
-  }
-  logout(){
+  logout() {
     window.sessionStorage.clear();
     window.location.reload();
   }
